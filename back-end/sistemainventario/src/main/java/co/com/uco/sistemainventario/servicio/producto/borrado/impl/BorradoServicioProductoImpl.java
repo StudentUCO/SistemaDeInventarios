4ef0fdaaafb9f3f0fd4e595b96184e1395066341
brayan.sepulveda4302 <brayan.sepulveda4302@soyuco.edu.co>
2021-10-12 23:40:21 -0500

    validación producto no este siendo utilizado en un inventario a la hora de eliminar

--- back-end/sistemainventario/src/main/java/co/com/uco/sistemainventario/servicio/producto/borrado/impl/BorradoServicioProductoImpl.java
+++ back-end/sistemainventario/src/main/java/co/com/uco/sistemainventario/servicio/producto/borrado/impl/BorradoServicioProductoImpl.java
@@ -4,19 +4,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import co.com.uco.sistemainventario.entidad.Producto;
+import co.com.uco.sistemainventario.persistencia.InventarioRepository;
 import co.com.uco.sistemainventario.persistencia.ProductoRepository;
 import co.com.uco.sistemainventario.servicio.producto.borrado.BorradoServicioProducto;
+import co.com.uco.sistemainventario.validador.excepcion.ExcepcionRegistroEnUso;
 
 @Service
 public class BorradoServicioProductoImpl implements BorradoServicioProducto {
+	
+	private static final String PRODUCTO_EN_INVENTARIO = "No se puese borrar el producto debido a que está siendo utilizado en un inventario";
 
 	@Autowired
 	private ProductoRepository productoRepository;
+	
+	@Autowired
+	private InventarioRepository inventarioRepository;
 
 	@Override
 	public Integer ejecutar(Producto producto) {
+		validarQueProductoNoEsteAsociadoAUnInventario(producto);
 		productoRepository.delete(producto);
 		return producto.getIdProducto();
+	}
+	
+	private void validarQueProductoNoEsteAsociadoAUnInventario(Producto producto) {
+		if (!inventarioRepository.findAllByProducto(producto).isEmpty()) {
+			throw new ExcepcionRegistroEnUso(PRODUCTO_EN_INVENTARIO);
+		}
 	}
 
 }

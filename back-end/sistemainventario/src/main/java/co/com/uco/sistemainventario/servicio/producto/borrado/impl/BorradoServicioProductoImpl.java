15f6a5517e22e73d7c9550dadab4d468243800db
jecq123 <juanestebanceballos@gmail.com>
2021-10-13 16:53:22 -0500

    Se crean las condiciones para la edicion y borrado de producto

--- back-end/sistemainventario/src/main/java/co/com/uco/sistemainventario/servicio/producto/borrado/impl/BorradoServicioProductoImpl.java
+++ back-end/sistemainventario/src/main/java/co/com/uco/sistemainventario/servicio/producto/borrado/impl/BorradoServicioProductoImpl.java
@@ -7,12 +7,15 @@ import co.com.uco.sistemainventario.entidad.Producto;
 import co.com.uco.sistemainventario.persistencia.InventarioRepository;
 import co.com.uco.sistemainventario.persistencia.ProductoRepository;
 import co.com.uco.sistemainventario.servicio.producto.borrado.BorradoServicioProducto;
+import co.com.uco.sistemainventario.validador.excepcion.ExcepcionNoExisteRegistro;
 import co.com.uco.sistemainventario.validador.excepcion.ExcepcionRegistroEnUso;
 
 @Service
 public class BorradoServicioProductoImpl implements BorradoServicioProducto {
 	
 	private static final String PRODUCTO_EN_INVENTARIO = "No se puese borrar el producto debido a que está siendo utilizado en un inventario";
+
+	private static final String NO_EXISTE_PRODUCTO = "No se puede borrar porque no existe el producto";
 
 	@Autowired
 	private ProductoRepository productoRepository;
@@ -22,9 +25,15 @@ public class BorradoServicioProductoImpl implements BorradoServicioProducto {
 
 	@Override
 	public Integer ejecutar(Producto producto) {
+		producto = buscarProducto(producto);
 		validarQueProductoNoEsteAsociadoAUnInventario(producto);
 		productoRepository.delete(producto);
 		return producto.getIdProducto();
+	}
+	
+	private Producto buscarProducto(Producto producto) {
+		return productoRepository.findById(producto.getIdProducto())
+				.orElseThrow(() -> new ExcepcionNoExisteRegistro(NO_EXISTE_PRODUCTO));
 	}
 	
 	private void validarQueProductoNoEsteAsociadoAUnInventario(Producto producto) {
--- back-end/sistemainventario/src/main/java/co/com/uco/sistemainventario/servicio/producto/edicion/impl/EdicionServicioProductoImpl.java
+++ back-end/sistemainventario/src/main/java/co/com/uco/sistemainventario/servicio/producto/edicion/impl/EdicionServicioProductoImpl.java
@@ -6,15 +6,28 @@ import org.springframework.stereotype.Service;
 import co.com.uco.sistemainventario.entidad.Producto;
 import co.com.uco.sistemainventario.persistencia.ProductoRepository;
 import co.com.uco.sistemainventario.servicio.producto.edicion.EdicionServicioProducto;
+import co.com.uco.sistemainventario.validador.excepcion.ExcepcionNoExisteRegistro;
 
 @Service
 public class EdicionServicioProductoImpl implements EdicionServicioProducto {
+	
+	private static final String NO_EXISTE_PRODUCTO = "No existe el producto";
+	
 	@Autowired
 	private ProductoRepository productoRepository;
 
 	@Override
 	public Integer ejecutar(Producto producto) {
-		productoRepository.save(producto);
+		Producto productoAnterior = buscarProducto(producto);
+		productoAnterior.setNombre(producto.getNombre());
+		productoAnterior.setCodigo(producto.getCodigo());
+		productoAnterior.setActivo(producto.isActivo());
+		productoRepository.save(productoAnterior);
 		return producto.getIdProducto();
+	}
+	
+	private Producto buscarProducto(Producto producto) {
+		return productoRepository.findById(producto.getIdProducto())
+				.orElseThrow(() -> new ExcepcionNoExisteRegistro(NO_EXISTE_PRODUCTO));
 	}
 }

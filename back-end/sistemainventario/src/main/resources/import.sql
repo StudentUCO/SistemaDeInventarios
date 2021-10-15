0e502b00965976afb18a06aa78f6374a1dec83a4
jecq123 <juanestebanceballos@gmail.com>
2021-10-15 15:14:14 -0500

    Se cambian los datos del login

--- back-end/sistemainventario/src/main/resources/import.sql
+++ back-end/sistemainventario/src/main/resources/import.sql
@@ -6,10 +6,10 @@ INSERT INTO inventario (idinventario,idproducto,cantidad) values (2,2,100)
 
 
 
-INSERT INTO usuario (id_usuario,nombre_usuario, password, enabled ) values (1,'diego','$2a$10$LXvl9cEhPNTO9pX9ApvPG.sPlrm0LuvxHDLmmw8gMFE6BZiwGCwhO', true)
-INSERT INTO usuario (id_usuario,nombre_usuario, password, enabled ) values (2,'anonimo','$2a$10$JHfTc8j5g1ZiSgzy4H1h8uFOO1uR3opBhxjRT/P/c1BHCnUgNqIwi', true)
+INSERT INTO usuario (id_usuario,nombre_usuario, password, enabled ) values (1,'juan','$2a$10$LXvl9cEhPNTO9pX9ApvPG.sPlrm0LuvxHDLmmw8gMFE6BZiwGCwhO', true)
+INSERT INTO usuario (id_usuario,nombre_usuario, password, enabled ) values (2,'juanes','$2a$10$67.bRA3bwhfWYIixkwZl3O/KzLjUB8jzQH7azaAVZ.nKf488wAM5q', true)
 
 INSERT INTO roles (id_rol,nombre_rol) values (1,'ROLE_ADMIN')
 INSERT INTO roles (id_rol,nombre_rol) values (2,'ROLE_CONSULTA')
 INSERT INTO users_authorities (id_usuario, id_rol ) values (1,1)
-INSERT INTO users_authorities (id_usuario, id_rol ) values (2,2)	
+INSERT INTO users_authorities (id_usuario, id_rol ) values (2,1)	
